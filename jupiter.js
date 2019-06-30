5a69e1e36c767fd273657c9e08fd106cbcce275b
JdeJ <jorgedjuana@gmail.com>
2019-06-30 19:07:15 +0200

    ADDS setGrid and getGrid methods with tests

--- jupiter.js
+++ jupiter.js
@@ -1,8 +1,22 @@
 class Jupiter {
   constructor () {
-    this.grid = null;
-    this.initialPosition = null;
-    this.movement = null;
+    this.grid = undefined;
+    this.position = undefined;
+    this.movement = undefined;
+  }
+
+  setGrid (x, y) {
+    if (typeof x === 'number' && typeof y === 'number'){
+      this.grid = new Array(x);
+      for (let i=0; i<this.grid.length; i++) {
+        this.grid[i] = new Array(y);
+      }
+      return [this.grid.length, this.grid[0].length];
+    }
+  }
+
+  getGrid () {
+    return this.grid;
   }
 }
 
--- jupiter.test.js
+++ jupiter.test.js
@@ -1,11 +1,50 @@
 const Jupiter = require ('./jupiter');
 
+let newJupiter = null;
+beforeEach(() => {
+  newJupiter = new Jupiter();
+});
+
 // Check Jupiter initialization
+test('newJupiter must be an instance of Jupiter', () => {
+  expect(newJupiter).toBeInstanceOf(Jupiter);
+});
+
 test('Check Jupiter initialization.', () => {
-  let newJupiter = new Jupiter;
   expect(newJupiter).toEqual({
-    grid: null,
-    initialPosition: null,
-    movement: null,
+    grid: undefined,
+    position: undefined,
+    movement: undefined,
   });
 });
+
+// Check methods definition
+test('setGrid must be a function', () => {
+  expect(newJupiter.setGrid).toBeInstanceOf(Function);
+});
+
+test('getGrid must be a function', () => {
+  expect(newJupiter.getGrid).toBeInstanceOf(Function);
+});
+
+// Check getters and setters
+test('setGrid of 5x5 must be equal [5,5].', () => {
+  expect(newJupiter.setGrid(5, 5)).toEqual([5, 5]);
+});
+
+test('setGrid of NaN must be undefined.', () => {
+  expect(newJupiter.setGrid('j', null)).toBeUndefined();
+});
+
+test('getGrid of 5x5 must be', () => {
+  newJupiter.setGrid(5, 5);
+  expect(Array.isArray(newJupiter.getGrid())).toBeTruthy();
+});
+
+test('Check setPosition.', () => {});
+
+test('Check getPosition.', () => {});
+
+test('Check setMovement.', () => {});
+
+test('Check getMovement.', () => {});
